38f987f5318733d993f92701e35dafc28d47c44b
gw709198558 <gw709198558@163.com>
2019-05-09 11:12:49 +0800

    "配置"

---
+++ ejavashop-service-impl/src/main/java/com/ejavashop/service/impl/share/ShareServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ejavashop.service.impl.share;
+
+import com.ejavashop.service.share.ShareService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShareServiceImpl implements ShareService {
+}
---
+++ ejavashop-service/src/main/java/com/ejavashop/service/share/ShareService.java
@@ -0,0 +1,8 @@
+package com.ejavashop.service.share;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ShareService {
+
+}
